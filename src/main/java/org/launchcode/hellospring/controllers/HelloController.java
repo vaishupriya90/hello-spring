db3813f85e1fb3363112d41f001fedde634a3f12
vaishu <vaishupriya90@gmail.com>
2021-02-18 00:47:02 -0600

    Added few codes to try conditionals in template

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,11 +1,13 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
     // responds to get requests at "/hello"
 //    @GetMapping("hello")
@@ -16,36 +18,47 @@ public class HelloController {
     // responds to post requests at "/goodbye"
     //after refactoring this path will be hello/goodbye--> since we have declared the starting of the path in the top under requestMApping
     @GetMapping("goodbye")
-    public String goodBye(){
+    @ResponseBody
+
+    public String goodBye() {
         return "Good Bye, Spring!";
     }
 
-    @RequestMapping(value = "hellogoodbye",method = {RequestMethod.GET,RequestMethod.POST} )
-    public String helloGoodBye(@RequestParam String name){
-        return "Hello, "+ name+"!!";
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloGoodBye(@RequestParam String name, Model model) {
+        String greetingString = "Welcome, "+ name + "!";
+        model.addAttribute("greeting",greetingString);
+        return "hello";
     }
 
     //handles query parameters in the form of /hello?name=value-->this value can be anything
     @GetMapping()
-    public String helloQueryParameter(@RequestParam String name){
-        return "Hello, "+ name + "!";
+    @ResponseBody
+    public String helloQueryParameter(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
+
     //Handles path parameter in the form of /hello/name-->name is a variable which should match the parameter's variable name
     @GetMapping("{name}")
-    public String helloPathParameter(@PathVariable String name){
-    //this name variable should match the path variable
-        return "Hollla, "+name+"!";
+    public String helloPathParameter(@PathVariable String name,Model model) {
+        //this name variable should match the path variable
+        String greetingString = "Hollla, " + name + "!";
+        model.addAttribute("greeting",greetingString);
+        return "hello";
     }
     //Handles a form
     @GetMapping("form")
-    public String helloForm(){
-        return "<html>" +
-                "<body>" +
-                "<form action='hellogoodbye' method='post'>" +
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet Me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+    public String helloForm() {
+        return "form";
+    }
+    @GetMapping("hello-forLoop")
+    public String helloForLoop(Model model){
+        List<String> namesList = new ArrayList<>();
+        namesList.add("vaishu");
+        namesList.add("kalai");
+        namesList.add("aaradhya");
+        namesList.add("arav");
+        model.addAttribute("names",namesList);
+        return "hello-forLoop";
     }
 }
